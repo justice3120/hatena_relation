7e4057919da4bf566cff289a131358be4263da52
Masayoshi Sakamoto <masayoshi.sakamoto@dena.com>
2016-03-30 15:15:45 +0900

    fix R path on production

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,14 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ENV['R_HOME'] ||= case RbConfig::CONFIG['host_os']
-                  when /linux/; '/usr/local/lib64/R'
-                  when /darwin/; '/Library/Frameworks/R.framework/Resources'
-                  end
+case Rails.env
+  when 'production' then
+    ENV['R_HOME'] ||= '/app/vendor/R/bin'
+  when 'staging' then
+    ENV['R_HOME'] ||= '/app/vendor/R/bin'
+  else
+    ENV['R_HOME'] ||= case RbConfig::CONFIG['host_os']
+                        when /linux/; '/usr/local/lib64/R'
+                        when /darwin/; '/Library/Frameworks/R.framework/Resources'
+                      end
+end
